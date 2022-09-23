8347af684a3613f308b79ca250c94a36b74c7e10
zqqq77 <114237699+zqqq77@users.noreply.github.com>
2022-09-23 22:59:39 +0800

    Update AllConfig.java

--- src/main/java/com/pt/vx/config/AllConfig.java
+++ src/main/java/com/pt/vx/config/AllConfig.java
@@ -6,54 +6,11 @@ import com.pt.vx.domain.User;
 
 import java.util.ArrayList;
 import java.util.List;
-/**
- * 内置的模板有：
- * 在微信模板里添加{{xxx.DATA}}
- *
- * 个人信息相关：
- * userName 昵称
- * holdDay 距离某天的天数
- * yourBirthDay 生日
- * myBirthDay 对象生日
- * loveDay 纪念日倒计时
- *
- * 天气相关：
- * date 日期
- * week 星期
- * weatherDay（1，2，3，4） 白天天气
- * temperatureDay（1，2，3，4）白天温度
- * windDay（1，2，3，4）白天风向
- * powerDay（1，2，3，4）白天风力
- * weatherNight（1，2，3，4） 晚上天气
- * temperatureNight（1，2，3，4） 晚上温度
- * windNight（1，2，3，4） 晚上风向
- * powerNight（1，2，3，4） 晚上风力
- * weatherNow 现在天气
- * temperatureNow 现在温度
- * windNow 现在风向
- * powerNow 现在风力
- * humidityNow 现在湿度
- *
- * 其他：（1、2、3、4......每100字一个，如tianGou超过了一百个字的话：{{tianGou.DATA}}{{tianGou1.DATA}}）
- * otherInfo 额外提示
- * randomRead 精彩短句
- * worldRead 世界新闻
- * tianGou 舔狗日记
- * qingHua 情话
- * english 励志英语
- * historyToday 历史的今天
- * horoscope 星座解析
- * randomInfo 随机额外类型消息
- * wozairenjian 我在人间凑日子散文
- * miyu 谜语
- * poetry 诗句
- * dongman 动漫
- * story 小说
- */
+
 public class AllConfig {
-    public static final String VxAppId = "微信的APPID";
-    public static final String VxAppSecret = "微信的密钥";
-    public static final String WeatherKey = "高德地图key";
+    public static final String VxAppId = "wx678f6e98f3af7fdf";
+    public static final String VxAppSecret = "581496ec129cf577fc2597d528680251";
+    public static final String WeatherKey = "1b9baf6427ceef0d0cf5ae5af64ef74e";
 
     //基本类型消息
     public static final boolean open_info = true; //是否开启个人情况 true 为开启， false 为不开启
@@ -88,27 +45,21 @@ public class AllConfig {
     private static void init(){
         //如果要多个人的话，就复制这个一遍，然后填写里面的内容。这里默认两个人,大伙应该是两个人吧（笑）
         userList.add(getUser(
-                "这个人扫码后的微信号",//扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "这个人的称呼",//咋称呼这个人
-                new BirthDay(1999,8,11,false),  //这个人的生日，最后的这个true/false，如果是过公历生日就写false，如果是过农历生日写true
-                new BirthDay(1999,2,15,true), //这个人对象的生日，最后的这个true/false，如果是过公历生日就写false，如果是过农历生日写true
-                LocalDate.of(2020,7,8),//这个人的纪念日起点，比如说在一起的那一天
-                "江苏省南京市玄武区",//这个人的详细地址
-                "南京",//这个人在的城市
-                "微信消息模板ID"));//要给这个人发送的模板ID
+                "ov9Nj6nsEo1oo5L-ZkUo5P_um-hA",//扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "七七",//咋称呼这个人
+                "广东省深圳市龙岗区",//这个人的详细地址
+                "深圳",//这个人在的城市
+                "X2kBELSk_H-6U0EmmVDxwV9z-GWfjgv3ZAY_tu-aJ7M"));//要给这个人发送的模板ID
 
         userList.add(getUser(
-                "这个人扫码后的微信号",
-                "这个人的名字",
-                new BirthDay(1999,2,15,true),  //这个人的生日，最后的这个true/false，如果是过公历生日就写false，如果是过农历生日写true
-                new BirthDay(1999,8,11,false), //这个人对象的生日，最后的这个true/false，如果是过公历生日就写false，如果是过农历生日写true
-                LocalDate.of(2020,7,8),//这个人的纪念日
-                "江苏省南京市玄武区",//这个人的详细地址
-                "南京",//这个人在的城市
-                "微信消息模板ID"));//要给这个人发送的模板ID
+                "ov9Nj6jShJZZZLs9VNIUII8ZQ24I",
+                "纸包鱼OvO"
+                "广东省深圳市盐田区",//这个人的详细地址
+                "深圳",//这个人在的城市
+                "X2kBELSk_H-6U0EmmVDxwV9z-GWfjgv3ZAY_tu-aJ7M"));//要给这个人发送的模板ID
 
-        
     }
+
 
     public static final List<User> userList = new ArrayList<>();
     static {
